ea86e4dfdf14d2e5c79d991d3e81594a8c75f268
CarlW194 <carl.white194@gmail.com>
2014-12-07 00:06:54 +0000

    NumberOddEven.java
    This file takes in an integer and checks if it is an even or an odd number and prints out the appropriate response.

---
+++ NumberOddEven.java
@@ -0,0 +1,24 @@
+  /** Algorithm
+   *
+   *1 declare variables
+   *2. make an if else statement that determines if the number is odd or even.
+   *
+   */
+   public class NumberOddEven
+   {
+   	public static void main(String args[])
+   	{
+   		//declaring variables
+   		int num1=228;
+
+   		// if else statement
+   		if(num1%2==0)
+   		{
+   			System.out.println("number " +num1+" is an even number");
+   			   		}
+        else
+        {
+        	System.out.println("number" +num1+"is an odd number");
+        }
+   	}
+   }
