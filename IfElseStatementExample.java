3b5240b5662426a2ca9ebdbe798ca25fd86bb513
CarlW194 <carl.white194@gmail.com>
2014-12-07 00:09:48 +0000

    IfElseStatementExample.java
    This file checks if the number is greater than the others and prints out either true or false.

---
+++ IfElseStatementExample.java
@@ -0,0 +1,19 @@
+public class Question6
+{
+	public static void main(String args[])
+	{
+	//declare variables
+	boolean x=true, y=false;
+	int num1=6, num2=8, num3=9, num4=4;
+
+	//true false statement
+	if(num1>num4 & num2>num3)
+		{
+System.out.println("The answer is "+x);
+}
+else
+{
+System.out.println("The answer is "+y);
+}
+	}
+}
