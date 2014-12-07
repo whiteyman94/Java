240872bd01fec27c89157844ac8d793537008e2f
CarlW194 <carl.white194@gmail.com>
2014-12-07 00:14:50 +0000

    MultipleOperatorSum.java
    This file takes in four numbers and uses multiple operators to print out the result.

---
+++ MultipleOperatorSum.java
@@ -0,0 +1,12 @@
+public class Question6E
+{
+public static void main(String args[])
+{
+// Declare nad initialise variables
+int num=0, num1=9, num2=5, num3=4;
+
+//result of equation
+num=num1+num2*num3/num3;
+System.out.println("The answer is " + num);
+}
+}
