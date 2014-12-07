97f8032a413fec69cd473cba91fef57dec12994c
CarlW194 <carl.white194@gmail.com>
2014-12-07 00:00:23 +0000

    ISBNCheck.java
    This file takes in a string and checks to see if it is an ISBN number and returns the correct statement if correct and an invalid ISBN response if not.

---
+++ ISBNCheck.java
@@ -0,0 +1,28 @@
+public class Question7
+{
+	public static void main(String args[])
+	{
+	//Declare and initialise variables
+		String s1=new String("0-123-45678-9");
+
+
+	//If else statement that shows if the string is a valid ISBN
+		if(s1.length()==13}
+		{
+
+			for(int x=0;x<s1.length();x++)
+			{
+				char c=s1.charAt(13)
+
+				if(c>=0 & c<=9)
+				{
+					System.out.println("This String is and ISBN number");
+				}
+
+				else
+				{
+					System.out.println("The number is not ISBN");
+				}
+			}
+		}
+}
