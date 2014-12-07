b75fd8d4f9b802b3b45ba3d30946ba7a0f031399
CarlW194 <carl.white194@gmail.com>
2014-12-07 00:01:45 +0000

    DescendingLoop.java
    This file is an example of a descending loop using a while loop and prints out the numbers in a descending loop.

---
+++ DescendingLoop.java
@@ -0,0 +1,22 @@
+/* Algorithm
+ *
+ *1. declare variables
+ *2. start the number from 100
+ *3. use a while-loop operation to get the number to the number 2 in descending order
+ *
+ */
+ public class DescendingLoop
+ {
+ 	public static void main(String args[])
+ 	{
+ 		// declare variables
+ 		int count = 100;
+
+ 		// while loop statement
+ 		while (count>=2)
+ 		{
+ 		System.out.println(" Count is " + count);
+ 		count=count-2;
+ 		}
+  	}
+ }
