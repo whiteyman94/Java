b85bdf12fd596cfabcb7e193b3779dc36d4c1eee
CarlW194 <carl.white194@gmail.com>
2014-12-07 00:13:14 +0000

    GreaterThanExample.java
    This file checks if the numbers are greater than the specified numbers using an or statement and prints out wither true or false.

---
+++ GreaterThanExample.java
@@ -0,0 +1,19 @@
+public class Question6B
+{
+public static void main (String args[])
+{
+//Declare and initailise variables
+boolean x=true, y=false;
+int num1=6, num2=20, num3=15;
+
+//True or false statement
+if(num1>15 | num3<20)
+		{
+System.out.println("The answer is " + x);
+}
+else
+{
+System.out.println("The answer is "+ y);
+}
+	}
+}
