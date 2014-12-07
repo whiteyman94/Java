91523e7a82eece85d8b0cea1a56d5679c34affc9
CarlW194 <carl.white194@gmail.com>
2014-12-07 00:04:42 +0000

    OperatorExample.java
    This file is an example of how java interprets the operaters in the compile stage.

---
+++ OperatorExample.java
@@ -0,0 +1,17 @@
+
+public class ExampleExam
+{
+    public static void main (String [] args)
+    {
+
+  //declare and initilaise variables
+ int a=2,b=3;
+int c=a++ + ++b;
+System.out.println(c);
+
+
+  }
+
+    }
+
+
